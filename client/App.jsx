fb28fdc4a0b8b2c1e864eac479119a6d8f3fc9bd
Olivia Hodel <ohodel16@gmail.com>
2023-09-24 16:04:29 -0400

    Moves client folder

---
+++ client/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import ProductsContainer from './components/ProductsContainer';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [received, setReceived] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [page, setPage] = useState({
+    current: 1,
+    previous: 'disabled',
+    next: 'active',
+  });
+
+  const getProducts = async (page) => {
+    try {
+      const productsRaw = await fetch(`/products?page=${page}`);
+      const productsParsed = await productsRaw.json();
+
+      setProducts(productsParsed.products);
+      setTotalCount(productsParsed.total_count);
+      setReceived(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const changePage = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    let targetText = target.innerText;
+
+    let newPage;
+    if (targetText === 'Next') newPage = page.current + 1;
+    else if (targetText === 'Previous') newPage = page.current - 1;
+    else newPage = +targetText;
+    setPage({
+      current: newPage,
+      previous: newPage <= 1 ? 'disabled' : 'active',
+      next: newPage < Math.ceil(totalCount / 15) ? 'active' : 'disabled',
+    });
+
+    // Reset product state to render the loading symbol
+    setReceived(false);
+    setProducts([]);
+
+    // Get the next page of products
+    getProducts(newPage);
+
+    // Scroll to top of page
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    getProducts(1);
+  }, []);
+
+  return (
+    <div className='h-100 d-flex flex-column align-items-center'>
+      <h1
+        className='h1 display-3 pt-5 pb-1'
+        style={{ color: '#2b33ff', fontWeight: 'bold' }}
+      >
+        Universe of Birds
+      </h1>
+      <h3 className='h3 pb-5 text-muted'>Product Details</h3>
+      <ProductsContainer
+        productData={products}
+        received={received}
+        totalCount={totalCount}
+        getProducts={getProducts}
+        changePage={changePage}
+        page={page}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/components/ProductsContainer.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+import Pagination from './Pagination';
+
+const ProductsContainer = (props) => {
+  // Map received product information to an array of product card components
+  const productCards = [];
+
+  for (let i = 0; i < props.productData.length; i++) {
+    const currProduct = props.productData[i];
+
+    // Handle when no image is sent
+    if (currProduct.image) {
+      productCards.push(
+        <ProductCard
+          title={currProduct.name}
+          image={currProduct.image.src}
+          total_orders={currProduct.total_orders}
+          price={currProduct.price}
+          total_value={currProduct.total_value}
+          total_inventory={currProduct.total_inventory}
+          type={currProduct.type}
+        />
+      );
+    } else {
+      productCards.push(
+        <ProductCard
+          title={currProduct.name}
+          total_orders={currProduct.total_orders}
+          price={currProduct.price}
+          total_value={currProduct.total_value}
+          total_inventory={currProduct.total_inventory}
+        />
+      );
+    }
+  }
+
+  return (
+    <div className='container'>
+      {/* If products have not been received yet, return the loading symbol */}
+      {!productCards.length && !props.received ? (
+        <div
+          style={{ height: '150px' }}
+          className='d-flex flex-column justify-content-center pb=5 align-items-center'
+        >
+          <div className='spinner-border text-primary' role='status'></div>
+          <p className='text-primary'>Loading...</p>
+        </div>
+      ) : (
+        <div className='flex justify-content-center'>
+          <div className='row justify-content-center'>{productCards}</div>
+          <Pagination changePage={props.changePage} page={props.page} totalCount={props.totalCount} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductsContainer;
